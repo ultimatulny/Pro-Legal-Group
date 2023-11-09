4bd37f1d20ee52856072bc87552631376eeccbbf
ultimatulny <cod.alexandr@yandex.ru>
2023-11-09 17:28:05 +0300

    ver 0.4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,14 +10,26 @@ const Header = () => {
                             <div className="logo"></div>
                         </Link>
                     </li>
-                    <li className="marginLeft90"><Link to="/about">About Us</Link></li>
-                    <li><Link to="/mission">Our Mission & Vision</Link></li>
-                    <li><Link to="/leadership">Leadership</Link></li>
+                    <li className="marginLeft90 pc-btn"><Link to="/about">About Us</Link></li>
+                    <li className="pc-btn"><Link to="/mission">Our Mission & Vision</Link></li>
+                    <li className="pc-btn"><Link to="/leadership">Leadership</Link></li>
                 </div>
                 <div>
-                    <li><Link to="/clients">Private Clients & Family Offices</Link></li>
-                    <li><Link to="/corporations">Corporations</Link></li>
+                    <li className="pc-btn"><Link to="/clients">Private Clients & Family Offices</Link></li>
+                    <li className="pc-btn"><Link to="/corporations">Corporations</Link></li>
+                    <label className="burger-menu">
+                        <input  type="checkbox"/>
+                        <span class="menu"> <span class="hamburger"></span> </span>
+                        <ul className="mobile-menu">
+                            <li> <Link to="/about">About Us</Link> </li>
+                            <li> <Link to="/mission">Our Mission & Vision</Link> </li>
+                            <li> <Link to="/leadership">Leadership</Link> </li>
+                            <li> <Link to="/clients">Private Clients & Family Offices</Link> </li>
+                            <li> <Link to="/corporations">Corporations</Link> </li>
+                        </ul>
+                    </label>
                 </div>
+               
             </ul>
         </header>
     )
