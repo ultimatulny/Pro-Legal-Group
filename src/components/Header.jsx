bc670e6104fd1544db6325028210bf369c78723a
ultimatulny <cod.alexandr@yandex.ru>
2023-11-10 23:22:10 +0300

    ver 0.5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,87 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
-    return(
-        <header>
-            <ul>
-                <div>
-                    <li>
-                        <Link to="/">
-                            <div className="logo"></div>
-                        </Link>
-                    </li>
-                    <li className="marginLeft90 pc-btn"><Link to="/about">About Us</Link></li>
-                    <li className="pc-btn"><Link to="/mission">Our Mission & Vision</Link></li>
-                    <li className="pc-btn"><Link to="/leadership">Leadership</Link></li>
-                </div>
-                <div>
-                    <li className="pc-btn"><Link to="/clients">Private Clients & Family Offices</Link></li>
-                    <li className="pc-btn"><Link to="/corporations">Corporations</Link></li>
-                  
-                </div>
-               
-            </ul>
-        </header>
-    )
-}
+  const [menuChecked, setMenuChecked] = useState(false);
 
-export {Header}
+  const handleLinkClick = () => {
+    setMenuChecked(false);
+  };
+
+  return (
+    <header>
+      <ul>
+        <div>
+          <li>
+            <Link to="/">
+              <div className="logo"></div>
+            </Link>
+          </li>
+          <li className="marginLeft90 pc-btn">
+            <Link to="/about">About Us</Link>
+          </li>
+          <li className="pc-btn">
+            <Link to="/mission">Our Mission & Vision</Link>
+          </li>
+          <li className="pc-btn">
+            <Link to="/leadership">Leadership</Link>
+          </li>
+        </div>
+        <div>
+          <li className="pc-btn">
+            <Link to="/clients">Private Clients & Family Offices</Link>
+          </li>
+          <li className="pc-btn">
+            <Link to="/corporations">Corporations</Link>
+          </li>
+
+          <div className="mobile-menu">
+            <input
+              type="checkbox"
+              id="show-menu"
+              role="button"
+              checked={menuChecked}
+              onChange={() => setMenuChecked(!menuChecked)}
+            />
+            <label htmlFor="show-menu">&#9776;</label>
+
+            <ul>
+              <li>
+                <Link to="/" onClick={handleLinkClick}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" onClick={handleLinkClick}>
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link to="/mission" onClick={handleLinkClick}>
+                  Our Mission & Vision
+                </Link>
+              </li>
+              <li>
+                <Link to="/leadership" onClick={handleLinkClick}>
+                  Leadership
+                </Link>
+              </li>
+              <li>
+                <Link to="/clients" onClick={handleLinkClick}>
+                  Private Clients & Family Offices
+                </Link>
+              </li>
+              <li>
+                <Link to="/corporations" onClick={handleLinkClick}>
+                  Corporations
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </ul>
+    </header>
+  );
+};
+
+export { Header };
