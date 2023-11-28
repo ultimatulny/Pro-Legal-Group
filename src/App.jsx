16c4cd9d7909a910565ce95044c99e26a513643c
ultimatulny <cod.alexandr@yandex.ru>
2023-11-28 21:40:20 +0300

    ver 0.7a

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import {Routes, Route} from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import {MainPage} from './pages/MainPage'
 import {AboutPage} from './pages/AboutPage'
@@ -12,19 +13,35 @@ import {NotfoundPage} from './pages/NotfoundPage'
 import { Layout } from "./components/Layout";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fakeLoading = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(fakeLoading);
+  }, []);
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<MainPage />}/>
-          <Route path="/about" element={<AboutPage />}/>
-          <Route path="/mission" element={<MissionPage />}/>
-          <Route path="/leadership" element={<LeadershipPage />}/>
-          <Route path="/clients" element={<PrivateclientsPage />}/>
-          <Route path="/corporations" element={<CorporationsPage />}/>
-          <Route path="*" element={<NotfoundPage />}/>
-        </Route>
-      </Routes>
+      {loading ? (
+        <div className="bannerBack">
+          <div className="bannerImg"></div>
+        </div>
+      ) : (
+        // Контент приложения, отображается после загрузки
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/mission" element={<MissionPage />} />
+            <Route path="/leadership" element={<LeadershipPage />} />
+            <Route path="/clients" element={<PrivateclientsPage />} />
+            <Route path="/corporations" element={<CorporationsPage />} />
+            <Route path="*" element={<NotfoundPage />} />
+          </Route>
+        </Routes>
+      )}
     </>
   );
 }
